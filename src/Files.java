a4b231b81a5ecfec2fadc675f14aaedfa4e318d9
Кристина <k.demchenko20203108@gmai.com>
2024-03-17 21:26:45 +0300

    Homework 3+4 (17.03.2024)

---
+++ src/Files.java
@@ -0,0 +1,26 @@
+public class Files {
+
+    String title;
+    String text;
+    String signature;
+    String watermark;
+
+    public Files(String title, String text, String signature, String watermark) {
+    this.title = title;
+    this.text = text;
+    this.signature = signature;
+    this.watermark = watermark;
+}
+    public String create() {
+        return String.format();
+    }
+
+    public String rewrite() {
+        return String.format();
+    }
+    public String delete(){
+    return String.format();
+}
+}
+
+
